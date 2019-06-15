5e886cd09e0bdaab0fdce61488a0b3e88351776a
Marcos Brizeno <marcos.uece.comp@gmail.com>
2019-06-15 10:35:56 -0500

    Basic Pong objects

---
+++ Assets/Ball.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float speed = 30;
+
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+    }
+
+    void OnCollisionEnter2D(Collision2D collisionInfo)
+    {
+        if (collisionInfo.gameObject.name == "RacketLeft")
+        {
+            float yDirection = hitFactor(transform.position, collisionInfo.transform.position, collisionInfo.collider.bounds.size.y);
+            Vector2 direction = new Vector2(1, yDirection).normalized;
+
+            GetComponent<Rigidbody2D>().velocity = direction * speed;
+        }
+        if (collisionInfo.gameObject.name == "RacketRight")
+        {
+            float yDirection = hitFactor(transform.position, collisionInfo.transform.position, collisionInfo.collider.bounds.size.y);
+            Vector2 direction = new Vector2(-1, yDirection).normalized;
+
+            GetComponent<Rigidbody2D>().velocity = direction * speed;
+        }
+    }
+
+    float hitFactor(Vector2 ballPosition, Vector2 racketPosition, float racketHeight)
+    {
+        return (ballPosition.y - racketPosition.y) / racketHeight;
+    }
+}
---
+++ Assets/MoveRacket.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRacket : MonoBehaviour
+{
+    public float speed = 30;
+    public string axisInput = "Vertical";
+
+    void FixedUpdate()
+    {
+        float verticalInput = Input.GetAxisRaw(axisInput);
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, verticalInput) * speed;
+    }
+}
