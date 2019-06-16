5cbbab3875e014f258c9054a7f0af842cb4385de
Marcos Brizeno <marcos.uece.comp@gmail.com>
2019-06-16 15:46:11 -0500

    Add simple scoring

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -1,14 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Ball : MonoBehaviour
 {
     public float speed = 30;
 
+    public GameObject scoreLeft;
+
+    public GameObject scoreRight;
+
     void Start()
     {
-        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+        GetComponent<Rigidbody2D>().velocity = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f)).normalized * speed;
     }
 
     void OnCollisionEnter2D(Collision2D collisionInfo)
@@ -27,10 +32,32 @@ public class Ball : MonoBehaviour
 
             GetComponent<Rigidbody2D>().velocity = direction * speed;
         }
+        if (collisionInfo.gameObject.name == "WallRight")
+        {
+            int newScore = intParseFast(scoreLeft.GetComponent<Text>().text) + 1;
+            scoreLeft.GetComponent<Text>().text = "" + newScore;
+        }
+        if (collisionInfo.gameObject.name == "WallLeft")
+        {
+            int newScore = intParseFast(scoreRight.GetComponent<Text>().text) + 1;
+            scoreRight.GetComponent<Text>().text = "" + newScore;
+        }
     }
 
     float hitFactor(Vector2 ballPosition, Vector2 racketPosition, float racketHeight)
     {
         return (ballPosition.y - racketPosition.y) / racketHeight;
     }
+
+    public static int intParseFast(string value)
+    {
+        int result = 0;
+        for (int i = 0; i < value.Length; i++)
+        {
+            char letter = value[i];
+            result = 10 * result + (letter - 48);
+        }
+        return result;
+    }
+
 }
